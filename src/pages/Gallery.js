80d3f18d1a57e92d70371d92c9561dcf746456a0
Abdulrahman Sadiq <61137642+master-keyCW@users.noreply.github.com>
2023-09-14 19:36:11 -0400

    Update Gallery.js

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import testi from "../image/testi_bg12525202.png"
+import trainer1 from '../image/instructors/1-1.jpg'
+import trainer2 from '../image/instructors/img-1.jpg'
+import trainer3 from '../image/instructors/img-4.jpg'
+import trainer4 from '../image/instructors/team-4.jpg'
+import trainer5 from '../image/instructors/team2201.jpg'
 
 const Gallery = () => {
   return (
@@ -10,11 +14,13 @@ const Gallery = () => {
               <p className='text-center text-white py-5 font-bold'>Fitmas - Gallery</p>
         </div>
       </section>
-      <div className=" bg-white">
-        <div className="hidden md:inline-flex my-10 md:ml-10 shadow rounded-sm w-full  justify-center">
-          <img src={testi} alt="Logo"/>
-        </div>
-      </div>
+      <section className='flex mx-auto my-6 py-4 '>
+        <img src={trainer5} alt='' className='p-5 w-64 rounded-lg'/>
+        <img src={trainer3} alt='' className='p-5  w-64 rounded-lg'/>
+        <img src={trainer2} alt='' className='p-5  w-64 rounded-lg'/>
+        <img src={trainer1} alt='' className='p-5  w-64 rounded-lg'/>
+        <img src={trainer4} alt='' className='p-5  w-64 rounded-lg'/>
+      </section>
     </div>
   )
 }
